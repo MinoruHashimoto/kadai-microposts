e1f6c2cee0f3dedcf7f611d7a6f10cde6a545fa1
minoru.sakamoto <minoru6432@gmail.com>
2020-12-16 00:41:11 +0000

    add edit update destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :edit]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -17,11 +17,32 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
+         session[:user_id] = @user.id
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
+  end
+  
+  def edit
+  end
+   
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:success] = 'ユーザを更新しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザは更新されませんでした。'
+      render :edit
+    end
+  end
+  
+  def destroy
+    current_user.destroy
+    flash[:success] = 'ユーザは退会しました。'
+    redirect_to signup_url
   end
 
   private
